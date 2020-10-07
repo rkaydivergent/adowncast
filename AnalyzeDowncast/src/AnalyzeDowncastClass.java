74d73e390ce2e2dc4bbf7ce7b702fb3f7fb94247
Rimsha's <Rimsha's@172.16.1.34>
2020-10-07 15:25:36 +0500

    first commit

---
+++ AnalyzeDowncast/src/AnalyzeDowncastClass.java
@@ -0,0 +1,8 @@
+
+public class AnalyzeDowncastClass {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub	
+			String[] strs = (String[])new Object[]{ "hello", "world" };
+		}
+	}
